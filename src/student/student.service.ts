75eb29fc93bf14e7230dcb03eca57fb2b77f8635
DaanAdr <parsa1815@outlook.com>
2025-03-12 11:21:37 +0100

    Added simple CRUD endpoint for students

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,26 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class StudentService {
-  create(createStudentDto: CreateStudentDto) {
-    return 'This action adds a new student';
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async create(createStudentDto: CreateStudentDto) {
+    return this.databaseService.student.create({
+      data: createStudentDto
+    });
   }
 
-  findAll() {
-    return `This action returns all student`;
+  async findAll() {
+    return this.databaseService.student.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} student`;
+  async findOne(id: number) {
+    return this.databaseService.student.findUnique({
+      where: {
+        id,     // The comma indicates the value has the same name as the key
+      }
+    });
   }
 
-  update(id: number, updateStudentDto: UpdateStudentDto) {
-    return `This action updates a #${id} student`;
+  async update(id: number, updateStudentDto: UpdateStudentDto) {
+    return this.databaseService.student.update({
+      where: {
+        id,
+      },
+      data: updateStudentDto
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} student`;
+  async remove(id: number) {
+    return this.databaseService.student.delete({
+      where: {
+        id,     // The comma indicates the value has the same name as the key
+      }
+    });
   }
 }
