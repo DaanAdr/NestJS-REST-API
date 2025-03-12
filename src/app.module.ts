22d9ba139598142bbf111582bdd19e15b0686d93
DaanAdr <parsa1815@outlook.com>
2025-03-12 11:09:54 +0100

    Added validation rules to PUT and POST DTO

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { DatabaseService } from './database/database.service';
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, StudentModule],
   controllers: [AppController],
   providers: [AppService, DatabaseService],
 })
