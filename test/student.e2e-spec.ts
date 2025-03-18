ca0dc2394395ca84dd41f3fa73a56256ad70fe17
DaanAdr <parsa1815@outlook.com>
2025-03-18 14:04:02 +0100

    Added E2E test for GET endpoint

--- test/student.e2e-spec.ts
+++ test/student.e2e-spec.ts
@@ -2,9 +2,13 @@ import * as request from 'supertest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
+import { DatabaseService } from '../src/database/database.service';
+import { CreateStudentDto } from '../src/student/dto/create-student.dto';
+import { GetStudentDto } from '../src/student/dto/get-student.dto';
 
 describe('StudentsController E2E Tests', () => {
   let app: INestApplication;
+  let databaseService: DatabaseService;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -13,18 +17,47 @@ describe('StudentsController E2E Tests', () => {
       .compile();   // Creates testing environment
 
       app = moduleFixture.createNestApplication();
+      databaseService = app.get<DatabaseService>(DatabaseService);
       await app.init();
   });
 
+  beforeEach(async () => {
+    // Reset or seed the database before each test
+    await resetDatabase();
+
+    // Seed
+    const studentOne = new CreateStudentDto;
+    studentOne.name = "Markus";
+
+    const studentTwo = new CreateStudentDto;
+    studentTwo.name = "Elias";
+
+    await databaseService.student.createMany({
+      data: [studentOne, studentTwo]
+    })
+  });
+
   // This is an actual test case
-  it(`/GET students`, () => {
-    return request(app.getHttpServer())
+  it(`/GET students`, async () => {
+    // Arrange
+    const expectedNames = ['Markus', 'Elias'];
+
+    // Act
+    const response = await request(app.getHttpServer())
       .get('/student')
-      .expect(200)
-      .expect([]);
+      .expect(200);
+
+    // Assert
+    const returnedNames = response.body.map(student => student.name);
+    expect(returnedNames).toEqual(expect.arrayContaining(expectedNames));
   });
 
   afterAll(async () => {
     await app.close();
   });
+
+  async function resetDatabase() {
+    // Clear all data from the database
+    await databaseService.student.deleteMany(); 
+  }
 });
